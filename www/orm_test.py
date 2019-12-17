83274e204e9bea0eb95a5ff6e5c3a67d1b1269cd
hyman213 <hyman213@163.com>
2019-12-17 15:37:06 +0800

    feat(web-framework): 新增web框架编写
    新增web框架编写

--- www/orm_test.py
+++ www/orm_test.py
@@ -4,7 +4,7 @@ from models import User
 
 def test():
     print('test start')
-    yield from orm.create_pool(user='root', password='root', database='py_awesome')
+    yield from orm.create_pool(user='root', password='root', db='py_awesome')
     print('test start - step 2')
     user = User(name='Hyman', email='hyman213@163.com', passwd='123456', image='about:blank')
     print('test start - step 3')
