af4f00bec84fb6d2aa0e34112775b08ecb7555a9
hyman213 <hyman213@163.com>
2019-12-19 14:45:28 +0800

    feat(signin): 新增用户登录功能
    新增用户登录功能

--- www/handlers.py
+++ www/handlers.py
@@ -12,6 +12,7 @@ from apis import Page, APIValueError, APIError
 
 import time, re, hashlib, json
 
+
 def get_page_index(page_str):
     p = 1
     try:
@@ -22,9 +23,10 @@ def get_page_index(page_str):
         p = 1
     return p
 
+
 @get('/')
 async def index(request):
-    print('*'*20)
+    print('*' * 20)
     # users = await User.findAll()
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     blogs = [
@@ -37,6 +39,7 @@ async def index(request):
         'blogs': blogs
     }
 
+
 # 注册正则校验
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
@@ -45,11 +48,25 @@ _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 
+
 @get('/register')
 def register():
     return {
         '__template__': 'register.html'
     }
+
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+
+'''
+用户注册
+'''
+
 
 @post('/api/register')
 def api_register_user(*, email, name, password):
@@ -60,11 +77,12 @@ def api_register_user(*, email, name, password):
     if not password or not _RE_SHA1.match(password):
         raise APIValueError('password')
     users = yield from User.findAll('email=?', [email])
-    if len(users)>0:
+    if len(users) > 0:
         raise APIError('register:failed', 'email', 'Email is already in use')
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid, password)
-    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='')
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+                image='')
     yield from user.save()
     # make session cookie
     r = web.Response()
@@ -73,6 +91,37 @@ def api_register_user(*, email, name, password):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+
+@post('/api/authenticate')
+def authenticate(*, email, password):
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email', 'Invalid email.')
+    if not password or not _RE_SHA1.match(password):
+        raise APIValueError('password', 'Invalid password.')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist')
+    user = users[0]
+    # check password
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(password.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('password', 'Invalid password.')
+    # set cookie
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+'''
+用户cookie
+'''
 
 
 def user2cookie(user, max_age):
@@ -86,8 +135,6 @@ def user2cookie(user, max_age):
     return '-'.join(L)
 
 
-
-
 @get('/api/users')
 def api_get_users(*, page='1'):
     page_index = get_page_index(page)
@@ -99,4 +146,3 @@ def api_get_users(*, page='1'):
     for u in users:
         u.passwd = '******'
     return dict(page=p, users=users)
-
